b658cceff435d16833baa3413b66925188026759
Matheus Ramalho de Lima <101842192+pitoko01@users.noreply.github.com>
2024-04-05 20:32:31 -0300

    faculdade

---
+++ exercicios/faculdade/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var limparBtn = document.getElementById('limpar-btn');
+    limparBtn.addEventListener('click', limparFormulario);
+  });
+
+
+function limparFormulario() {
+    document.getElementById("form-contato").reset();
+    console.log('apagou')
+  }
